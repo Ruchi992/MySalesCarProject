1701e53c2e58d3d0ad9b812690d435b9ed1db8e0
Industry Connect <Industry Connect@DESKTOP-GHDLD74.Home>
2020-11-13 12:12:18 +0000

    add sql

---
+++ src/main/java/moylishmotors/database/sales_stockTable.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 11, 2020 at 11:57 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sales_stock`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_stockTable`
+--
+-- Error reading structure for table sales_stock.sales_stockTable: #1146 - Table 'sales_stock.sales_stocktable' doesn't exist
+-- Error reading data for table sales_stock.sales_stockTable: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sales_stock`.`sales_stockTable`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
